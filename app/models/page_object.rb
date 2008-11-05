cdcf2be73fb181bfa76c2320270189034808ec2c
Matthew Moore <matt@thrivesmart.com>
2008-11-04 16:56:27 -0800

    pagination

--- app/models/page_object.rb
+++ app/models/page_object.rb
@@ -3,7 +3,7 @@ class PageObject < ActiveRecord::Base
   self.caching_default = "data_update[events]" #[in :forever, :page_update, :any_page_update, 'data_update[datetimes]', :never, 'interval[5]']
   self.caching_scope_default = 'request_param[page]' #[in '', 'request_param[month]'] - a new cached object will be created for each scope value
 
-  attr_accessor :page, :events
+  attr_accessor :page, :total_page_count, :events
   
   def upcoming_events
     @upcoming_events ||= self.events.reject {|e| e.end <= Time.now}.sort { |x,y| x.start <=> y.start }
@@ -14,13 +14,14 @@ class PageObject < ActiveRecord::Base
   end
   
   def fetch_events(attrs = {})
-    self.page = page_query_parameters[:page]
+    self.page = page_query_parameters[:page] || 1
     
     self.events = self.organization.find_data(:events, 
       :include => [:url, :start, :end, :name, :description, :picture], 
       :conditions => {  :start => { :page => page }, 
                         :end => { :page => page } })
-                        
+
+    self.total_page_count = events.first.respond_to?(:page_count) ? events.first.page_count : 1 
     parse_event_times
   end
   
